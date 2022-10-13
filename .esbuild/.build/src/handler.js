3f2a5b641275dbbc054b61dffec69be4dc8451dc
guilhermebarroso-sys <guibarrosodeoliveira5@gmail.com>
2022-10-12 23:44:21 -0300

    Feat: 🎊 First commit

---
+++ .esbuild/.build/src/handler.js
@@ -0,0 +1,13 @@
+// src/handler.ts
+module.exports.hello = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v3.0! Your function executed successfully!!!!"
+      },
+      null,
+      2
+    )
+  };
+};
